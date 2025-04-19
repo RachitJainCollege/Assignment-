de5b9d57253d4a80a157cff6529864fa78caa478
RachitJainCollege <rachitjain.ai27@jecrc.ac.in>
2025-04-19 16:58:10 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,10 @@
-
+a = input("Enter first Number:" )
+b = input("Enter second Number:" )
+Addition=a+b
+Subtraction=a-b
+Multiplication=a*b
+Division=a/b
+print ("Addition =",Addition)
+print ("Subtraction =",Subtraction)
+print ("Multiplication =",Multiplication)
+print ("Division =",Division)
